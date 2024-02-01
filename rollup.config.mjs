1e053a2818a023800d130c7a7a51a7f3e5e53fda
Joseph <JPMeehan5@Gmail.com>
2024-01-31 16:10:15 -0800

    Simplified rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,7 @@
-import scss from 'rollup-plugin-scss';
-
 export default {
   input: './talent-psionics.mjs',
   output: {
     file: './talent-psionics-compiled.mjs',
     format: 'esm',
   },
-  plugins: [
-    scss({
-      // include: ['./styles/*.scss'],
-      fileName: 'talent-psionics.css',
-      // failOnError: true,
-    }),
-  ],
 };
